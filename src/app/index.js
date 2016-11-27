754d6e868b8e476b0129122a2cce3da202aebf7a
Alberto~Ferioli <albertobebz@gmail.com>
2016-11-27 20:31:04 +0000

    add the basic functionality

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,51 @@
-console.log("It works!");
+// importing the React libraries
+import React from "react";
+import { render } from "react-dom";
+
+// importing the Components
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
+    onGreet() {
+        alert("alert");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset">
+                        <Header homeLink={this.state.homeLink}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset">
+                        <Home
+                            name={"Bob"}
+                            initialAge={23}
+                            greet={this.onGreet}
+                            changeLink ={this.onChangeLinkName.bind(this)}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, document.getElementById("app"));
